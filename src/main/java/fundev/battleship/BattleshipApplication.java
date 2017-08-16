6a4e31bb3ed35f5f7ea5a683078395ed76cb449d
Krzysztof Krawczyk <krzysztof0krawczyk@gmail.com>
2017-08-16 21:27:55 +0200

    #1 Adding home controller with simple hello message.

--- src/main/java/fundev/battleship/BattleshipApplication.java
+++ src/main/java/fundev/battleship/BattleshipApplication.java
@@ -2,9 +2,18 @@ package fundev.battleship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class BattleshipApplication {
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Welcome to Battleship";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BattleshipApplication.class, args);
